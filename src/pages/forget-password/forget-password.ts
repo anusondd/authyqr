204371b947028e4f85136a9d920990a2ee889998
DataBasess <anusondd@gmail.com>
2018-01-10 10:46:08 +0700

    forgetpass

--- src/pages/forget-password/forget-password.ts
+++ src/pages/forget-password/forget-password.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ForgetPasswordPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../models/User';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { TostServiceProvider } from '../../providers/tost-service/tost-service';
 
 @IonicPage()
 @Component({
@@ -15,11 +11,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ForgetPasswordPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private Auth:AngularFireAuth,
+    public Tost:TostServiceProvider
+  ) {
   }
+
+  user:User ={
+    email:'anusondd@gmail.com',
+    password:'21519097'
+  };
+  massage:string;
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ForgetPasswordPage');
+  }
+
+  Resetpassword(user:User){
+    this.Auth.auth.sendPasswordResetEmail(user.email).then(result=>{
+      console.log('pass',result);
+      
+    }).catch(error=>{
+      console.log('error',error);
+    })
+
   }
 
 }
