c98ba3982fff8902d42446c87ac0e1dd65f3552d
DataBasess <anusondd@gmail.com>
2018-01-13 22:58:21 +0700

    verify-phonenumber

--- src/pages/verify-phonenumber/verify-phonenumber.ts
+++ src/pages/verify-phonenumber/verify-phonenumber.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the VerifyPhonenumberPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'verify-phonenumber.html',
 })
 export class VerifyPhonenumberPage {
+  phoneNumber:FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private formBuilder: FormBuilder
+  ) {
+    this.phoneNumber = this.formBuilder.group({
+      number:['',Validators.compose([
+          Validators.required, 
+          Validators.minLength(10), 
+          Validators.maxLength(10)
+        ])
+      ]
+    })
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VerifyPhonenumberPage');
+        
+  }
+
+  sendOTP(phoneNumber:FormGroup){
+    let Numberp = phoneNumber.value;
+    console.log(Numberp.number)
+    
   }
 
 }
